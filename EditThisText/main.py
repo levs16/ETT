c72692014eec7446d8a211c2d15cc4b2913636d7
Sokolov Lev Sergeevich <89748410+levs16@users.noreply.github.com>
2023-10-13 15:31:26 +0300

    Replace the main folder

--- EditThisText/main.py
+++ EditThisText/main.py
@@ -1,11 +1,15 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QAction, QFontDialog
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QFileDialog, QAction, QFontDialog, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5 import QtGui
+
 
 class TextEditor(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
         self.initUI()
 
     def initUI(self):
@@ -13,55 +17,60 @@ class TextEditor(QMainWindow):
         self.setCentralWidget(self.textEdit)
         self.statusBar()
 
-        newFile = QAction(QIcon('new.png'), 'New', self)
+        newFile = QAction(QIcon('icons\\new.png'), 'New', self)
         newFile.setShortcut('Ctrl+N')
         newFile.setStatusTip('Create a new file')
         newFile.triggered.connect(self.newFile)
 
-        openFile = QAction(QIcon('open.png'), 'Open', self)
+        openFile = QAction(QIcon('icons\\open.png'), 'Open', self)
         openFile.setShortcut('Ctrl+O')
         openFile.setStatusTip('Open a file')
         openFile.triggered.connect(self.openFile)
 
-        saveFile = QAction(QIcon('save.png'), 'Save', self)
+        saveFile = QAction(QIcon('icons\\save.png'), 'Save', self)
         saveFile.setShortcut('Ctrl+S')
         saveFile.setStatusTip('Save the file')
         saveFile.triggered.connect(self.saveFile)
 
-        saveAsFile = QAction(QIcon('saveas.png'), 'Save As', self)
+        saveAsFile = QAction(QIcon('icons\\saveas.png'), 'Save As', self)
         saveAsFile.setShortcut('Ctrl+Shift+S')
         saveAsFile.setStatusTip('Save the file as')
         saveAsFile.triggered.connect(self.saveAsFile)
 
-        exitApp = QAction(QIcon('exit.png'), 'Exit', self)
+        exitApp = QAction(QIcon('icons\\exit.png'), 'Exit', self)
         exitApp.setShortcut('Ctrl+Q')
         exitApp.setStatusTip('Exit the application')
         exitApp.triggered.connect(self.close)
 
-        copyText = QAction(QIcon('copy.png'), 'Copy', self)
+        copyText = QAction(QIcon('icons\\copy.png'), 'Copy', self)
         copyText.setShortcut('Ctrl+C')
         copyText.setStatusTip('Copy the selected text')
         copyText.triggered.connect(self.textEdit.copy)
 
-        pasteText = QAction(QIcon('paste.png'), 'Paste', self)
+        pasteText = QAction(QIcon('icons\\paste.png'), 'Paste', self)
         pasteText.setShortcut('Ctrl+V')
         pasteText.setStatusTip('Paste the copied text')
         pasteText.triggered.connect(self.textEdit.paste)
 
-        cutText = QAction(QIcon('cut.png'), 'Cut', self)
+        cutText = QAction(QIcon('icons\\cut.png'), 'Cut', self)
         cutText.setShortcut('Ctrl+X')
         cutText.setStatusTip('Cut the selected text')
         cutText.triggered.connect(self.textEdit.cut)
 
-        selectAllText = QAction(QIcon('selectall.png'), 'Select All', self)
+        selectAllText = QAction(QIcon('icons\\selectall.png'), 'Select All', self)
         selectAllText.setShortcut('Ctrl+A')
         selectAllText.setStatusTip('Select all the text')
         selectAllText.triggered.connect(self.textEdit.selectAll)
 
-        fontChange = QAction(QIcon('font.png'), 'Font', self)
+        fontChange = QAction(QIcon('icons\\font.png'), 'Font', self)
         fontChange.setShortcut('Ctrl+F')
         fontChange.setStatusTip('Change the font of the text')
         fontChange.triggered.connect(self.fontChange)
+
+        aboutWin = QAction(QIcon('icons\\about.png'), 'About', self)
+        aboutWin.setShortcut('Ctrl+Shift+I')
+        aboutWin.setStatusTip('Display the About window')
+        aboutWin.triggered.connect(self.aboutWinOpen)
 
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
@@ -77,6 +86,9 @@ class TextEditor(QMainWindow):
         editMenu.addAction(cutText)
         editMenu.addAction(selectAllText)
         editMenu.addAction(fontChange)
+
+        helpMenu = menubar.addMenu('&Help')
+        helpMenu.addAction('About', self.aboutWinOpen)
 
         toolbar = self.addToolBar('Tools')
         toolbar.setMovable(True)
@@ -94,21 +106,25 @@ class TextEditor(QMainWindow):
         toolbar.addAction(fontChange)
 
         self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('EditThisText v0.4')
+        self.setWindowTitle('EditThisText v0.5')
         self.show()
 
     def newFile(self):
         self.textEdit.clear()
 
     def openFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        try:
+            fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
 
-        if fname[0]:
-            f = open(fname[0], 'r')
+            if fname[0]:
+                f = open(fname[0], 'r')
 
-            with f:
-                data = f.read()
-                self.textEdit.setText(data)
+                with f:
+                    data = f.read()
+                    self.textEdit.setText(data)
+        
+        except Exception as e:
+            print(f"log> Handled the error: {e}")
 
     def saveFile(self):
         fname = QFileDialog.getSaveFileName(self, 'Save file', '/home')
@@ -135,6 +151,24 @@ class TextEditor(QMainWindow):
 
         if ok:
             self.textEdit.setFont(font)
+
+    def aboutWinOpen(self):
+        self.about = QMainWindow()
+        self.about.setWindowTitle('About Text Editor')
+        self.about.setFixedSize(300, 200)
+
+        self.iconLabel = QLabel(self.about)
+        self.iconLabel.setPixmap(QPixmap('icons\\icon.png'))
+        self.iconLabel.setAlignment(Qt.AlignCenter)
+        self.iconLabel.setGeometry(0, 0, 300, 150)
+
+        self.textLabel = QLabel(self.about)
+        self.textLabel.setText('Aboba Inc. All Rights Reserved 2023.')
+        self.textLabel.setOpenExternalLinks(True)
+        self.textLabel.setAlignment(Qt.AlignCenter)
+        self.textLabel.setGeometry(0, 150, 300, 50)
+
+        self.about.show()
 
 
 if __name__ == '__main__':
